0e99b76b3d06e58124ef17b1c884315206c8ffe6
Elijah Peake <elijah.peake@gmail.com>
2020-05-01 01:05:37 -0400

    error handling

--- gpu_graham_scan.h
+++ gpu_graham_scan.h
@@ -5,6 +5,7 @@
 
 #include <errno.h>
 #include <fstream>
+#include <stdio.h>
 #include <string>
 #include <vector>
 #include <cmath> // trig functions, pow
@@ -26,6 +27,13 @@ template <class Num_Type> struct Point {
 };
 
 /*
+ * prints helpful error diagnostics
+ */
+#define PRINT_ERROR(message) fprintf(stderr, "Error: function %s, file %s, line %d.\n%s", __func__, __FILE__, __LINE__, message);
+#define PRINT_ERROR_LOCATION() fprintf(stderr, "Error: function %s, file %s, line %d.\n", __func__, __FILE__, __LINE__);
+
+
+/*
  * Used to read in a file of Point to be stored
  * as a vector.
  */
@@ -42,23 +50,33 @@ template <class Num_Type> class GrahamScanSerial {
 
       infile.open(filename_);
       if (errno != 0) {
+        PRINT_ERROR_LOCATION();
         perror("infile.open");
         exit(EXIT_FAILURE);
       }
       
-      // get number of points
+      // get number of points from first line of file
       getline(infile, curr_line);
       if (infile.fail()) {
+        PRINT_ERROR_LOCATION();
         perror("getline");
         exit(EXIT_FAILURE);
       }
 
-      points_ = new Point<Num_Type>[stoi(curr_line)];
+      int total_points = stoi(curr_line);
+
+      if (total_points < 1) {
+        PRINT_ERROR("Less than one point in input file\n");
+        exit(EXIT_FAILURE);
+      }
+
+      points_.resize(total_points);
 
       // process each line individually of the file
       while (!infile.eof()) {
         getline(infile, curr_line);
         if (infile.fail()) {
+          PRINT_ERROR_LOCATION();
           perror("getline");
           exit(EXIT_FAILURE);
         }
@@ -77,18 +95,24 @@ template <class Num_Type> class GrahamScanSerial {
           idx_min_y_ = idx;
         }
 
-        points_[idx] = current_point;
+        points_.push_back(current_point);
         idx++;
+      }
+
+      if (total_points != idx) {
+        PRINT_ERROR("Incorrect number of points specified by file\n");
+        exit(EXIT_FAILURE);
       }
 
       infile.close();
       if (infile.fail()) {
-          perror("infile.close");
-          exit(EXIT_FAILURE);
+        PRINT_ERROR_LOCATION();
+        perror("infile.close");
+        exit(EXIT_FAILURE);
       }
     };
 
-    ~GrahamScanSerial() { delete[] points_; }
+    ~GrahamScanSerial() { }
 
     /*
      * filename of points to be read in
@@ -103,46 +127,48 @@ template <class Num_Type> class GrahamScanSerial {
     /*
      * all of our points from the file
      */
-    Point<Num_Type> * points_;
+    vector<Point<Num_Type> > points_;
 
-    
-    Point<Num_Type> GetMinYPoint() {
+    /*
+     * Returns the leftmost point with the minumum y value of our points_ vector
+     */
+    Point<Num_Type> GetMinYPoint(void) {
       return points_[idx_min_y_];
     };
 
 
-  /* 
-   * calculate polar angles between two points
-   * parallel to x-axis is 0
-   * 
-   * args:
-   * returns:
-   */
-  float PolarAngle(Point<Num_Type> p0, Point<Num_Type> p1) const {
-    float x_diff = p1.x - p0.x;
-    float y_diff = p1.y - p0.y;
+    /* 
+    * calculate polar angles between two points
+    * parallel to x-axis is 0
+    * 
+    * args:
+    * returns:
+    */
+    float PolarAngle(Point<Num_Type> p0, Point<Num_Type> p1) const {
+      float x_diff = p1.x - p0.x;
+      float y_diff = p1.y - p0.y;
 
-    float hypotenuse = hypotf(x_diff, y_diff);
-    return acos(x_diff/hypotenuse); // use cosine so the function is always defined
-  }
-
-
-  /* 
-   * does the ordering self, p1, p2 create a non-left turn
-   * 
-   * args:
-   * returns:
-   */
-  bool NonLeftTurn(Point<Num_Type> p0, Point<Num_Type> p1, Point<Num_Type> p2) const {
-    float firstAngle = PolarAngle(p0, p1);
-    float secondAngle = PolarAngle(p0, p2);
-
-    if (abs(firstAngle - secondAngle) < kPI) {
-      return secondAngle <= firstAngle;
-    } else {
-      return firstAngle <= secondAngle;
+      float hypotenuse = hypotf(x_diff, y_diff);
+      return acos(x_diff/hypotenuse); // use cosine so the function is always defined
     }
-  }
+
+
+    /* 
+    * does the ordering self, p1, p2 create a non-left turn
+    * 
+    * args:
+    * returns:
+    */
+    bool NonLeftTurn(Point<Num_Type> p0, Point<Num_Type> p1, Point<Num_Type> p2) const {
+      float firstAngle = PolarAngle(p0, p1);
+      float secondAngle = PolarAngle(p0, p2);
+
+      if (abs(firstAngle - secondAngle) < kPI) {
+        return secondAngle <= firstAngle;
+      } else {
+        return firstAngle <= secondAngle;
+      }
+    }
   
   private:
     GrahamScanSerial(void);
