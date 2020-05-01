b1c2ed96f68b3011c5642b58d889aec289443ff2
Elijah Peake <elijah.peake@gmail.com>
2020-05-01 01:22:25 -0400

    error handling cont

--- gpu_graham_scan.h
+++ gpu_graham_scan.h
@@ -29,7 +29,7 @@ template <class Num_Type> struct Point {
 /*
  * prints helpful error diagnostics
  */
-#define PRINT_ERROR(message) fprintf(stderr, "Error: function %s, file %s, line %d.\n%s", __func__, __FILE__, __LINE__, message);
+#define PRINT_ERROR(message) fprintf(stderr, "Error: function %s, file %s, line %d.\n%s\n", __func__, __FILE__, __LINE__, message);
 #define PRINT_ERROR_LOCATION() fprintf(stderr, "Error: function %s, file %s, line %d.\n", __func__, __FILE__, __LINE__);
 
 
@@ -44,70 +44,83 @@ template <class Num_Type> class GrahamScanSerial {
      * Constructor reads through the file, populating points_ and idx_min_y_
      */
     GrahamScanSerial(const char * filename) : filename_(filename) {
-      string curr_line;
-      ifstream infile;
-      int idx = 0;
-
-      infile.open(filename_);
-      if (errno != 0) {
-        PRINT_ERROR_LOCATION();
-        perror("infile.open");
-        exit(EXIT_FAILURE);
-      }
       
-      // get number of points from first line of file
-      getline(infile, curr_line);
-      if (infile.fail()) {
-        PRINT_ERROR_LOCATION();
-        perror("getline");
-        exit(EXIT_FAILURE);
-      }
+      // only function that throws is stoi/stod so need try/catch
+      try {
+        string curr_line;
+        ifstream infile;
+        int idx = 0;
 
-      int total_points = stoi(curr_line);
-
-      if (total_points < 1) {
-        PRINT_ERROR("Less than one point in input file\n");
-        exit(EXIT_FAILURE);
-      }
-
-      points_.resize(total_points);
-
-      // process each line individually of the file
-      while (!infile.eof()) {
+        infile.open(filename_);
+        if (errno != 0) {
+          PRINT_ERROR_LOCATION();
+          perror("infile.open");
+          exit(EXIT_FAILURE);
+        }
+        
+        // get number of points from first line of file
         getline(infile, curr_line);
         if (infile.fail()) {
           PRINT_ERROR_LOCATION();
           perror("getline");
           exit(EXIT_FAILURE);
         }
-        int comma = curr_line.find(',');
-        string first_num = curr_line.substr(0, comma);
-        string second_num = curr_line.substr(comma + 1, curr_line.length());
 
-        Point<Num_Type> current_point;
-        current_point.x = (Num_Type) stod(first_num);
-        current_point.y = (Num_Type) stod(second_num);
-
-        // update the current minumim point's index
-        if (idx == 0
-            || (current_point.y == GetMinYPoint().y && current_point.x < GetMinYPoint().x)
-            || current_point.y < GetMinYPoint().y) {
-          idx_min_y_ = idx;
+        int total_points = stoi(curr_line);
+        if (errno != 0) {
+          std::cout<<"hi";
         }
 
-        points_.push_back(current_point);
-        idx++;
-      }
+        if (total_points < 1) {
+          PRINT_ERROR("Less than one point in input file");
+          exit(EXIT_FAILURE);
+        }
 
-      if (total_points != idx) {
-        PRINT_ERROR("Incorrect number of points specified by file\n");
+        points_.resize(total_points);
+
+        // process each line individually of the file
+        while (!infile.eof()) {
+          getline(infile, curr_line);
+          if (infile.fail()) {
+            PRINT_ERROR_LOCATION();
+            perror("getline");
+            exit(EXIT_FAILURE);
+          }
+          int comma = curr_line.find(',');
+          string first_num = curr_line.substr(0, comma);
+          string second_num = curr_line.substr(comma + 1, curr_line.length());
+
+          Point<Num_Type> current_point;
+          current_point.x = (Num_Type) stod(first_num);
+          current_point.y = (Num_Type) stod(second_num);
+
+          // update the current minumim point's index
+          if (idx == 0
+              || (current_point.y == GetMinYPoint().y && current_point.x < GetMinYPoint().x)
+              || current_point.y < GetMinYPoint().y) {
+            idx_min_y_ = idx;
+          }
+
+          points_.push_back(current_point);
+          idx++;
+        }
+
+        if (total_points != idx) {
+          PRINT_ERROR("Incorrect number of points specified by file");
+          exit(EXIT_FAILURE);
+        }
+
+        infile.close();
+        if (infile.fail()) {
+          PRINT_ERROR_LOCATION();
+          perror("infile.close");
+          exit(EXIT_FAILURE);
+        }
+      } catch (const std::out_of_range& oor) {
+        PRINT_ERROR(oor.what());
         exit(EXIT_FAILURE);
-      }
-
-      infile.close();
-      if (infile.fail()) {
-        PRINT_ERROR_LOCATION();
-        perror("infile.close");
+      } catch (const std::invalid_argument& ia) {
+        PRINT_ERROR(ia.what());
         exit(EXIT_FAILURE);
       }
     };
